ddb9b33c0a3028e6b9d694e7fd67a36418bc0212
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2026-06-30 18:48:01 +0900

    init ios

---
+++ ios/gpscamera/gpscameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpscameraApp.swift
+//  gpscamera
+//
+//  Created by ray on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct gpscameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
